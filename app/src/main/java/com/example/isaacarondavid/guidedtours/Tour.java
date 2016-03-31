321e0920b83608d4108bdf4e9a2ec55aec4c48ba
Isaac Mast <isaac.k.mast@gmail.com>
2016-03-31 13:22:28 -0400

    Added Tour class for initial DB setup

---
+++ app/src/main/java/com/example/isaacarondavid/guidedtours/Tour.java
@@ -0,0 +1,37 @@
+package com.example.isaacarondavid.guidedtours;
+
+public class Tour {
+
+	private int id;
+	private String name;
+
+	public Tour() {
+	
+	}
+
+	public Tour(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
