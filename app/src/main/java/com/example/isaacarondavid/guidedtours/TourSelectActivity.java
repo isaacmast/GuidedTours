c22d38f830b316f766381a07473fb93d33ae5480
DavidNester <davidnester@Davids-MacBook-Pro.local>
2016-04-20 01:21:51 -0400

    Added Tour for mennonite colleges
    Added second tour that can be used when testing the new class

--- app/src/main/java/com/example/isaacarondavid/guidedtours/TourSelectActivity.java
+++ app/src/main/java/com/example/isaacarondavid/guidedtours/TourSelectActivity.java
@@ -36,13 +36,21 @@ public class TourSelectActivity extends Activity implements View.OnClickListener
         db.insertDestination(new Destination(db.getTour("EMU").getId(),4,"SC","This building was newly renovated in 2015 and has all of our science labs.",(float)38.470007,(float) -78.878113));
         db.insertDestination(new Destination(db.getTour("EMU").getId(), 5, "Library", "Sadie Hartler Library: where students go to study.", (float) 38.470272, (float) -78.878997));
 
+        db.insertTour(new Tour(2, "Mennonite Colleges", "These are the five mennonite colleges in the US"));
+        db.insertDestination(new Destination(db.getTour("Mennonite Colleges").getId(), 6, "EMU", "Eastern Menonite University, Harrisonburg, VA", (float) 38.472000, (float) -78.877306));
+        db.insertDestination(new Destination(db.getTour("Mennonite Colleges").getId(), 7, "Bluffton", "Bluffton University, Bluffton, OH", (float) 40.896393, (float) -83.896934));
+        db.insertDestination(new Destination(db.getTour("Mennonite Colleges").getId(), 8, "Goshen", "Goshen College, Goshen, IN", (float) 41.564414, (float) -85.827363));
+        db.insertDestination(new Destination(db.getTour("Mennonite Colleges").getId(), 9, "Bethel", "Bethel College, Bethel, KS", (float) 38.074617, (float) -97.342347));
+        db.insertDestination(new Destination(db.getTour("Mennonite Colleges").getId(), 10, "Hesston", "Hesston College, Hesston, KS", (float) 38.13324, (float) -97.432913));
+
+
         tours = db.getTourNames();
 
-        Toast.makeText(this,"here",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,tours[0],Toast.LENGTH_LONG).show();
         ArrayAdapter<String> t = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tours);
         t.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dummyButton.setText("EMU");
-        //spinner.setAdapter(t);
+        dummyButton.setText("Mennonite Colleges");
+        //spinner.setAdapter(t); //the line that is causing the problem
     }
     public void onListItemClick(ListView parent, View v, int position,
                                 long id){
@@ -55,7 +63,7 @@ public class TourSelectActivity extends Activity implements View.OnClickListener
     @Override
     public void onClick(View v) {
         Intent map = new Intent(getApplicationContext(),MapsActivity.class);
-        map.putExtra("Tour Name","EMU");
+        map.putExtra("Tour Name","Mennonite Colleges");
         startActivity(map);
     }
 
